c690e3df1f11b5e9fc88cda5c04de84b0d0b8edd
Steven Huang <szteven3@gmail.com>
2017-09-22 18:33:19 +0800

    add neseted directory path to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'pry'
 question_files = File.join("questions","**","*.rb")
+question_files_2 = File.join("questions","**","**","*.rb")
 Dir.glob(question_files).each { |f| require_relative  File.join("..",f) }
+Dir.glob(question_files_2).each { |f| require_relative  File.join("..",f) }
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
