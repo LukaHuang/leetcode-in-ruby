0b5f9763c13fd4b6822e7a7da0c21edf06d772df
Steven Huang <szteven3@gmail.com>
2017-09-22 14:31:19 +0800

    add 231 power of two

--- questions/number/231_power_of_two.rb
+++ questions/number/231_power_of_two.rb
@@ -1,13 +1,10 @@
 def is_power_of_two(n)
-   while n > 3
-    n = n % 2
+   temp = 1
+   while( tmp <= n)
+     if tmp == n
+       return true
+     end
+     tmp *= 2
    end
-   return n == 0
+   return false
 end
-
-# 16 / 2 = 8
-# 8 / 2 = 4
-# 4 / 2 = 2
-# 2 / 2 = 1
-# 17 / 2 = 8 .. 1
-
